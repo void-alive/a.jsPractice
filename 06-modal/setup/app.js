e9f8c8c8316fc61463d5fbfeb7d32f2b14139fe7
void-alive <sja9284@gmail.com>
2025-01-09 12:47:19 +0900

    6장

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,11 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+$(document).ready(function () {
+  $(".modal-btn").on("click", function () {
+    $(".modal-overlay").addClass("open-modal");
+  });
+  $(".close-btn").on("click", function () {
+    $(".modal-overlay").removeClass("open-modal");
+  });
+});
