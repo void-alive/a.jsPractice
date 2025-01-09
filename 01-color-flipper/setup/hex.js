d36cdba808125204c7f95c6a7c1a68038463ba66
void-alive <sja9284@gmail.com>
2025-01-09 09:07:32 +0900

    hex.html, js 파일 수정

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+$(document).ready(function () {
+  $("#btn").on("click", function () {
+    let randomNumber = 0;
+
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      randomNumber = Math.floor(Math.random() * hex.length);
+      hexColor = hexColor + hex[randomNumber];
+    }
+
+    $("main").css("backgroundColor", hexColor);
+    $(".color").text(hexColor);
+    console.log(hexColor);
+  });
+});
