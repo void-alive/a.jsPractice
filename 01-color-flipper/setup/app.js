e25ec5e6f85bc18d9dcfbc09ca33a96accf6bd6a
void-alive <sja9284@gmail.com>
2025-01-09 09:01:41 +0900

    index.html, app.js 수정

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+$(document).ready(function () {
+  $("#btn").on("click", function () {
+    let randomNumber = 0;
+    randomNumber = Math.floor(Math.random() * 4);
+    $("main").css("backgroundColor",colors[randomNumber]);
+    $(".color").text(colors[randomNumber]);
+  });
+});
